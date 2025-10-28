bd4c538b7fb85b313c0496ce8f8cfe54142c2dd5
Olina Savčuková <olinkasavcuk@gmail.com>
2025-10-28 16:59:03 +0100

    List tabs styles

--- src/components/AddNewItemBtn.jsx
+++ src/components/AddNewItemBtn.jsx
@@ -2,7 +2,7 @@ function AddNewItemButton({ onClick }) {
   return (
     <button
       className="
-      btn btn-primary btn-md"
+      btn btn-primary btn-md px-24"
       onClick={onClick}
     >
       +Add new
--- src/components/ListTabs.jsx
+++ src/components/ListTabs.jsx
@@ -1,8 +1,22 @@
 function ListTabs({ activeTab, onTabChange }) {
   return (
-    <div>
-      <button onClick={() => onTabChange("incomplete")}>Incomplete</button>
-      <button onClick={() => onTabChange("all")}>All</button>
+    <div className="flex justify-center mx-10 mb-4 space-x-2">
+      <button
+        onClick={() => onTabChange("incomplete")}
+        className={`badge flex-1 py-3 ${
+          activeTab === "incomplete" ? "badge-primary" : "badge-ghost"
+        }`}
+      >
+        Incomplete
+      </button>
+      <button
+        onClick={() => onTabChange("all")}
+        className={`badge flex-1 py-3 ${
+          activeTab === "all" ? "badge-primary" : "badge-ghost"
+        }`}
+      >
+        All
+      </button>
     </div>
   );
 }
