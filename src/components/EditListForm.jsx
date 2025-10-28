9eb8b2449b2558eb29dbde492e5f723d9da91d81
Olina Savčuková <olinkasavcuk@gmail.com>
2025-10-28 18:09:42 +0100

    Edit list form component styles implemented

--- src/components/EditListForm.jsx
+++ src/components/EditListForm.jsx
@@ -12,26 +12,49 @@ function EditListForm({
   };
 
   return (
-    <>
-      <div>
-        <p>Edit list</p>
-      </div>
+    <div className="p-4 space-y-6">
+      <p className="text-2xl font-bold">Edit list</p>
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="listName">List name</label>
-        <input type="text" id="listName" value={value} onChange={onChange} />
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="form-control w-full">
+          <label htmlFor="listName" className="label">
+            <span className="label-text text-sm font-medium text-base-content text-opacity-80">
+              List name
+            </span>
+          </label>
 
-        <button type="submit">Save</button>
-        {isOwner && (
-          <button type="button" onClick={onMembers}>
-            Manage other users
+          <input
+            type="text"
+            id="listName"
+            value={value}
+            onChange={onChange}
+            className="input input-bordered w-full"
+          />
+        </div>
+
+        <div className="pt-2 space-y-2">
+          <button type="submit" className="btn btn-primary btn-md w-full">
+            Save
           </button>
-        )}
-        <button type="button" onClick={onCancel}>
-          Cancel
-        </button>
+          {isOwner && (
+            <button
+              type="button"
+              onClick={onMembers}
+              className="btn bg-base-200 btn-md w-full"
+            >
+              Manage other users
+            </button>
+          )}
+          <button
+            type="button"
+            onClick={onCancel}
+            className="btn bg-base-200 btn-md w-full"
+          >
+            Cancel
+          </button>
+        </div>
       </form>
-    </>
+    </div>
   );
 }
 
